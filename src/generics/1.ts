f748c68fde96f3540a1e6f89e93806e5712b35dc
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-06-28 21:34:30 +0300

    add

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -2,13 +2,13 @@
 // та повертає об'єкт з даними. Використовуйте Generics для типізації
 // повернутих даних.
 
-// import axios from "axios";
+import axios from "axios";
 
-// async function fetchData(url) {
-//   try {
-//     const response = await axios.get(url);
-//     return response.data;
-//   } catch (error) {
-//     throw new Error(`Error fetching from ${url}: ${error}`);
-//   }
-// }
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response: any = await axios.get<T>(url);
+    return response.data;
+  } catch (error) {
+    throw new Error(`Error fetching from ${url}: ${error}`);
+  }
+}
--- src/generics/2.ts
+++ src/generics/2.ts
@@ -3,18 +3,20 @@
 // Pick та generics для вказівки, що поля цих параметрів
 // належать AllType.Функція compare повинна повертати AllType.
 
-// type AllType = {
-//     name: string;
-//     position: number;
-//     color: string;
-//     weight: number
-//   }
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number;
+};
 
-//   function compare (top, bottom): AllType {
-//     return {
-//       name: top.name,
-//       color: top.color,
-//       position: bottom.position,
-//       weight: bottom.weight,
-//     }
-//   }
+type AllTypePick = Pick<AllType, "name" | "position" | "color" | "weight">;
+
+function compare(top: AllType, bottom: AllTypePick): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  };
+}
